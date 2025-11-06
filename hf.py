0ac08d96a248be9b4bd2a99c40a31f7092ee1678
Bobchenyx <chen.yixia@northeastern.edu>
2025-11-06 14:47:21 -0800

    initial commit

---
+++ hf.py
@@ -0,0 +1,14 @@
+# !pip install huggingface_hub hf_transfer
+import os
+# os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+from huggingface_hub import snapshot_download
+
+print("Starting download...")
+
+snapshot_download(
+    repo_id = "deepseek-ai/DeepSeek-V2-Lite",
+    local_dir = "deepseek-ai/DeepSeek-V2-Lite",
+)
+
+
+print("Download finished.")
