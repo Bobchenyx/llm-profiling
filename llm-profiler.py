6c9aeb2887e2c6fca8b852e7b75a9025da1282a7
Bobchenyx <chen.yixia@northeastern.edu>
2025-11-06 18:49:44 -0800

    fix device mapping for multi gpus & add Qwen3MoE example log

--- llm-profiler.py
+++ llm-profiler.py
@@ -23,9 +23,7 @@ def llm_input_constructor(batch_size, seq_len, tokenizer, device):
     return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": labels}
 
 
-with get_accelerator().device(0):
-    device = get_accelerator().current_device()
-    device_name = get_accelerator().current_device_name()
+def main():
 
     model_name = "./Qwen/Qwen3-30B-A3B-Instruct-2507"   # "./deepseek-ai/DeepSeek-V2-Lite-Chat"
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
@@ -45,7 +43,8 @@ with get_accelerator().device(0):
     seq_len = 128
     enable_profile = True
 
-    kwargs = llm_input_constructor(batch_size, seq_len, tokenizer, device)
+    first_device = next(model.parameters()).device
+    kwargs = llm_input_constructor(batch_size, seq_len, tokenizer, first_device)
     # use_cache=False
     kwargs["use_cache"] = False
 
@@ -63,44 +62,7 @@ with get_accelerator().device(0):
     else:
         outputs = model(**kwargs)
         print(outputs.loss.item())
+        
 
-
-# ====== Bert Example =====
-
-# from functools import partial
-# import torch
-# from transformers import BertForSequenceClassification, BertTokenizer
-# from deepspeed.profiling.flops_profiler import get_model_profile
-# from deepspeed.accelerator import get_accelerator
-
-
-# def bert_input_constructor(batch_size, seq_len, tokenizer):
-#     fake_seq = ""
-#     for _ in range(seq_len - 2):  # ignore the two special tokens [CLS] and [SEP]
-#       fake_seq += tokenizer.pad_token
-#     inputs = tokenizer([fake_seq] * batch_size,
-#                        padding=True,
-#                        truncation=True,
-#                        return_tensors="pt")
-#     labels = torch.tensor([1] * batch_size)
-#     inputs = dict(inputs)
-#     inputs.update({"labels": labels})
-#     return inputs
-
-
-# with get_accelerator().device(0):
-#     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-#     model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
-#     batch_size = 4
-#     seq_len = 128
-#     enable_profile = True
-#     if enable_profile:
-#       flops, macs, params = get_model_profile(
-#           model,
-#           kwargs=bert_input_constructor(batch_size, seq_len, tokenizer),
-#           print_profile=True,
-#           detailed=True,
-#       )
-#     else:
-#       inputs = bert_input_constructor((batch_size, seq_len), tokenizer)
-#       outputs = model(inputs)
+if __name__ == "__main__":
+    main()
